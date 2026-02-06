3e0792eda5b6c1fc093ad5fac1483644a2d4c601
Abdelrahman Ahmed <abdelrahmanahmed1923@gmail.com>
2026-02-06 18:36:08 +0200

    Feat: Login - Register And Logout

---
+++ src/main/java/com/security/login_and_registrationwithservletandjsp/LoginServlet.java
@@ -0,0 +1,68 @@
+package com.security.login_and_registrationwithservletandjsp;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        String uemail= request.getParameter("username");
+        String upwd = request.getParameter("password");
+        RequestDispatcher dispatcher = null;
+        HttpSession session = request.getSession();
+        Connection conn = null;
+
+        try{
+            Class.forName("org.postgresql.Driver");
+
+            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/registirationandlogin","postgres","AFHJ12&asd");
+
+            PreparedStatement pst = conn.prepareStatement("Select  * from users where uemail=? and upwd=?");
+            pst.setString(1, uemail);
+            pst.setString(2, upwd);
+
+            ResultSet rs = pst.executeQuery();
+
+            if(rs.next()){
+                session.setAttribute("name", rs.getString("username"));
+                dispatcher = request.getRequestDispatcher("index.jsp");
+            }
+            else{
+                request.setAttribute("status", "failed");
+                dispatcher = request.getRequestDispatcher("login.jsp");
+
+            }
+            dispatcher.forward(request,response);
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        finally{
+            try {
+                if(conn != null){
+                    conn.close();
+
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+
+
+
+
+    }
+
+}
---
+++ src/main/java/com/security/login_and_registrationwithservletandjsp/RegistirationServlet.java
@@ -0,0 +1,92 @@
+package com.security.login_and_registrationwithservletandjsp;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@WebServlet("/register")
+public class RegistirationServlet extends HttpServlet {
+
+
+        private String message;
+
+        public void init() {
+            message = "Hello World!";
+        }
+
+//        public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//            response.setContentType("text/html");
+//
+//            // Hello
+//            PrintWriter out = response.getWriter();
+//            out.println("<html><body>");
+//            out.println("<h1>" + message + "</h1>");
+//            out.println("</body></html>");
+//        }
+        public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+            String uname = request.getParameter("name");
+            String uemail = request.getParameter("email");
+            String upwd = request.getParameter("pass");
+            String uphone = request.getParameter("contact");
+            RequestDispatcher dispatcher = null;
+            Connection conn = null;
+
+            try{
+                Class.forName("org.postgresql.Driver");
+
+                conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/registirationandlogin","postgres","AFHJ12&asd");
+
+                PreparedStatement pst = conn.prepareStatement("insert into users (username , uemail , upwd , uphone)  values(?,?,?,?)");
+                pst.setString(1, uname);
+                pst.setString(2, uemail);
+                pst.setString(3, upwd);
+                pst.setString(4, uphone);
+
+                int rowCount = pst.executeUpdate();
+                dispatcher = request.getRequestDispatcher("registration.jsp");
+
+                if(rowCount > 0){
+                    request.setAttribute("status", "success");
+                }
+                else{
+                    request.setAttribute("status", "failed");
+                }
+                dispatcher.forward(request, response);
+
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+            finally{
+                try {
+                    if(conn != null){
+                        conn.close();
+
+                    }
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+
+
+
+
+        }
+
+
+
+        public void destroy() {
+        }
+
+}
